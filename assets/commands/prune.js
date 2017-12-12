7ef9b9192906c8007f1b62540992a41deaf97e53
Dave R <davetheprogrammer2@gmail.com>
2017-12-12 20:00:24 +0000

    old af code

--- assets/commands/prune.js
+++ assets/commands/prune.js
@@ -1,12 +1,12 @@
 exports.run = (client, msg, args) => {
 if (!msg.channel.permissionsFor(msg.author).hasPermission("MANAGE_MESSAGES")) {
-    msg.channel.sendMessage(":wastebasket: | You do not have the required permission to delete messages!").then(msg=>msg.delete(5000));
+    msg.channel.send(":wastebasket: | You do not have the required permission to delete messages!").then(msg=>msg.delete(5000));
     return;
   } else if (!msg.channel.permissionsFor(client.user).hasPermission("MANAGE_MESSAGES")) {
-    msg.channel.sendMessage(":wastebasket: | I do not have the required permission to delete messages. Please check your roles").then(msg=>msg.delete(5000));
+    msg.channel.send(":wastebasket: | I do not have the required permission to delete messages. Please check your roles").then(msg=>msg.delete(5000));
     return;
   }
   let messagecount = parseInt(args.join(" "));
     msg.channel.fetchMessages({limit : messagecount}).then(msgs => msg.channel.bulkDelete(msgs));
-        msg.channel.sendMessage(':wastebasket: | Messages have been deleted!').then(msg=>msg.delete(5000))
+        msg.channel.send(':wastebasket: | Messages have been deleted!').then(msg=>msg.delete(5000))
 }
